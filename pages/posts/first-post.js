53523e2bc711b13fdbd9ecb72255d54485db669c
Sandesh Dhoju <sandeshdhoju@gmail.com>
2023-01-30 15:22:50 +1100

    Assets, Metadata and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,24 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Page</title>
             </Head>
             <h1>First Post...</h1>
+            <Image
+                src='/images/profile.jpg'
+                alt='profile pic'
+                height={144}
+                width={144}
+            />
             <h2>
                 <Link href={"/"}>Back to Home</Link>
             </h2>
-        </>
+        </Layout>
     )
 }
